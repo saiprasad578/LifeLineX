b9bc1f9941d5926f223ec831264b6bec7048a557
sai prasad <saiprasad78v@gmail.com>
2026-04-12 11:59:58 +0530

    Implement goHome function and auto redirect
    This file handles the client-side functionality of the SOS Alert page. It defines the behavior of user interactions and automates navigation.
    
    The script includes a function that redirects the user back to the homepage when the button is clicked, as well as an optional automatic redirection after a few seconds.
    
    Key responsibilities:
    
    * Handle button click navigation
    * Implement automatic redirection logic
    * Enhance user experience with dynamic behavior

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// Function for button click
+function goHome() {
+    window.location.href = "index.html";
+}
+
+// Auto redirect after 5 seconds
+setTimeout(() => {
+    window.location.href = "index.html";
+}, 5000);
