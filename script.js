fd17175953de8ff440a5eb83daf62390a6afaed4
sai prasad <saiprasad78v@gmail.com>
2026-04-12 12:00:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Function for button click
-function goHome() {
-    window.location.href = "index.html";
-}
-
-// Auto redirect after 5 seconds
-setTimeout(() => {
-    window.location.href = "index.html";
-}, 5000);
